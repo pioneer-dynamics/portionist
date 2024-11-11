6bb0586340fa92761a3c638b3b64924e865741a5
Mathew Paret <mathewparet@gmail.com>
2024-11-12 09:50:39 +1100

    Added OG data

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import Logo from '../../static/logo.png';
 
 const props = defineProps({
     title: String,
@@ -29,7 +30,7 @@ const logout = () => {
 
 <template>
     <div>
-        <Head :title="title" />
+        <Head :title="title"/>
 
         <Banner />
 
