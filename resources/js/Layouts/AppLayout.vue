007131c8467940b2963ee1da012b9c902ea9a97b
Mathew Paret <mathewparet@gmail.com>
2024-11-12 09:55:12 +1100

    Removed use of unused component

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -7,7 +7,6 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import Logo from '../../static/logo.png';
 
 const props = defineProps({
     title: String,
@@ -30,7 +29,7 @@ const logout = () => {
 
 <template>
     <div>
-        <Head :title="title"/>
+        <Head :title="title" />
 
         <Banner />
 
