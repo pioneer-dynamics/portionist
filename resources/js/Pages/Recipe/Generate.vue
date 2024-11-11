ec55698ec69f72bcf654a2190b77cfefd4d3639f
Mathew Paret <mathewparet@gmail.com>
2024-11-12 07:46:21 +1100

    Fixed ui

--- resources/js/Pages/Recipe/Generate.vue
+++ resources/js/Pages/Recipe/Generate.vue
@@ -47,7 +47,7 @@
                             v-model="form.recipeType"
                             :options="['cocktail','food']"
                         />
-                        <PrimaryButton :disabled="form.processing" >
+                        <PrimaryButton :disabled="form.processing">
                             <span v-if="form.processing">
                                 <div role="status">
                                     <svg aria-hidden="true" class="mx-6 w-6 h-6 text-gray-200 animate-spin dark:text-gray-800 fill-branding-green-900" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
