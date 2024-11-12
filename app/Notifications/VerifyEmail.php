f593ef41aaecbff1acd906a9f6e3d2e2b33dbf39
Mathew Paret <mathewparet@gmail.com>
2024-11-12 23:24:45 +1100

    Linting

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -5,8 +5,6 @@ namespace App\Notifications;
 use Illuminate\Auth\Notifications\VerifyEmail as LaravelVerifyEmail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Notifications\Notification;
 
 class VerifyEmail extends LaravelVerifyEmail implements ShouldQueue
 {
