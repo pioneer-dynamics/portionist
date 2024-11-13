f9d82a31c7e710f26e89446d50fe0848b976317b
Mathew Paret <mathewparet@gmail.com>
2024-11-13 15:25:58 +1100

    mobile app login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\MobileAppUserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::name('api.')->group(function() {
+    Route::post('/login', [MobileAppUserController::class, 'login'])->name('login');
+
+    Route::middleware('auth:sanctum')->group(function() {
+        
+        Route::controller(MobileAppUserController::class)->group(function() {
+            Route::get('/user', 'user')->name('user');
+            Route::post('/logout', 'logout')->name('logout');
+        });
+    });
+});
