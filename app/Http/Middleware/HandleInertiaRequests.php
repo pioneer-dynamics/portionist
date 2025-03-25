fde0b7761aa90fde60fa0d00bc83e51bb6e93ccf
Mathew Paret <mathewparet@gmail.com>
2025-03-25 21:37:28 +1100

    Fix flash

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Middleware;
 
-use Illuminate\Http\Request;
 use Inertia\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -35,8 +36,10 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return array_merge(parent::share($request), [
-            //
+        return array_merge_recursive(parent::share($request), [
+            'jetstream' => [
+                'flash' => Session::get('flash'),
+            ]
         ]);
     }
 }
